f3d04e4bdfb2ca5c6a6dabe45a19c34cef8b02fa
Midacts <midactsmystery@gmail.com>
2013-04-05 17:30:21 -0300

    Update FindDirectoryACL.ps1

--- FindDirectoryACL.ps1
+++ FindDirectoryACL.ps1
@@ -5,6 +5,8 @@ $dir = "# DIRECTORY NAME"
 
 $Find = Get-Childitem -path "$Path" -recurse -force | Where-Object{$_.PSIsContainer} | Get-ACL | Select-Object Path -ExpandProperty Access | where { $_.IdentityReference -like "$Domain\$GROUP"} | select-object path,accesscontroltype,identityreference,isinherited | ogv
 
+# This is used to keep the windows open after the code is run.
 [System.Console]::Read()
 
+# This is just another way to use the code. Instead of displaying the results in OGV, it exports the results to a CSV.
 $Find = Get-Childitem -path "$Path" -recurse -force | Where-Object {$_.PSIsContainer} | Get-ACL | Select-Object Path -ExpandProperty Access | where { $_.IdentityReference -like "$Domain\$GROUP"} | select-object path,accesscontroltype,identityreference,isinherited | export-csv "$dir"
